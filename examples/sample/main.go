93c0baf6739e86efdf3e489c956e3ceba41c1fb5
0032 <yehao@duoshengbu.com>
2019-05-29 16:43:02 +0800

    rm http:// to support http or https in config

--- examples/sample/main.go
+++ examples/sample/main.go
@@ -8,16 +8,25 @@ import (
 )
 
 func main() {
-	// 通过默认根目录下的app.properties初始化agollo
-	err := agollo.InitWithDefaultConfigFile(
+	//// 通过默认根目录下的app.properties初始化agollo
+	//err := agollo.InitWithDefaultConfigFile(
+	//	agollo.WithLogger(agollo.NewLogger(agollo.LoggerWriter(os.Stdout))), // 打印日志信息
+	//	agollo.PreloadNamespaces("TEST.Namespace"),                          // 预先加载的namespace列表，如果是通过配置启动，会在app.properties配置的基础上追加
+	//	agollo.AutoFetchOnCacheMiss(),                                       // 在配置未找到时，去apollo的带缓存的获取配置接口，获取配置
+	//	agollo.FailTolerantOnBackupExists(),                                 // 在连接apollo失败时，如果在配置的目录下存在.agollo备份配置，会读取备份在服务器无法连接的情况下
+	//)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	agollo.Init(
+		os.Getenv("APOLLO_META"),
+		"dsb-server-wokamd8xNDDR8T6z",
 		agollo.WithLogger(agollo.NewLogger(agollo.LoggerWriter(os.Stdout))), // 打印日志信息
 		agollo.PreloadNamespaces("TEST.Namespace"),                          // 预先加载的namespace列表，如果是通过配置启动，会在app.properties配置的基础上追加
 		agollo.AutoFetchOnCacheMiss(),                                       // 在配置未找到时，去apollo的带缓存的获取配置接口，获取配置
-		agollo.FailTolerantOnBackupExists(),                                 // 在连接apollo失败时，如果在配置的目录下存在.agollo备份配置，会读取备份在服务器无法连接的情况下
+		agollo.FailTolerantOnBackupExists(),
 	)
-	if err != nil {
-		panic(err)
-	}
 
 	/*
 		通过指定配置文件地址的方式初始化
@@ -35,10 +44,10 @@ func main() {
 	fmt.Println("Name:", agollo.Get("Name"))
 
 	// 获取默认配置中cluster=default namespace=application key=Name的值，提供默认值返回
-	fmt.Println("YourConfigKey:", agollo.Get("YourConfigKey", agollo.WithDefault("YourDefaultValue")))
-
-	// 获取默认配置中cluster=default namespace=Test.Namespace key=Name的值，提供默认值返回
-	fmt.Println("YourConfigKey2:", agollo.Get("YourConfigKey2", agollo.WithDefault("YourDefaultValue"), agollo.WithNamespace("YourNamespace")))
+	fmt.Println("app.name:", agollo.Get("app.name", agollo.WithDefault("dddd")))
+	//
+	//// 获取默认配置中cluster=default namespace=Test.Namespace key=Name的值，提供默认值返回
+	//fmt.Println("YourConfigKey2:", agollo.Get("YourConfigKey2", agollo.WithDefault("YourDefaultValue"), agollo.WithNamespace("YourNamespace")))
 
 	// 获取namespace下的所有配置项
 	fmt.Println("Configuration of the namespace:", agollo.GetNameSpace("application"))
@@ -47,7 +56,7 @@ func main() {
 	// agollo初始化是带上agollo.AutoFetchOnCacheMiss()可选项的话
 	// 陪到非预加载的namespace，会去apollo缓存接口获取配置
 	// 未配置的话会返回空或者传入的默认值选项
-	fmt.Println(agollo.Get("Name", agollo.WithDefault("foo"), agollo.WithNamespace("TEST.Namespace1")))
+	//fmt.Println(agollo.Get("Name", agollo.WithDefault("foo"), agollo.WithNamespace("TEST.Namespace1")))
 
 	// 如果想监听并同步服务器配置变化，启动apollo长轮训
 	// 返回一个期间发生错误的error channel,按照需要去处理
