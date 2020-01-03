5e9b8a059ba9008ba6c760304ca5fe5714fcae1e
liuxingwang <liuxingwang@liuxingwangdeMacBook-Pro.local>
2020-01-03 11:54:59 +0800

    [format] 1. remove useless go.mod 2. modify sample code make it kind

--- examples/viper/main.go
+++ examples/viper/main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/shima-park/agollo/viper-remote"
+	remote "github.com/shima-park/agollo/viper-remote"
 	"github.com/spf13/viper"
 )
 
 func main() {
 	//agollo.Init("192.168.12.15:8080", "AppTest")
-	remote.SetAppID("AppTest")
+	remote.SetAppID("SampleApp")
 
 	app := viper.New()
 	test := viper.New()
@@ -21,8 +21,8 @@ func main() {
 
 	// 一个namespace相当于一个配置文件
 	// 需要不同的viper对象进行读取管理，否则会有key冲突等问题
-	app.AddRemoteProvider("apollo", "192.168.12.15:8080", "application")
-	test.AddRemoteProvider("apollo", "192.168.12.15:8080", "TEST.Namespace1")
+	app.AddRemoteProvider("apollo", "localhost:8080", "application")
+	test.AddRemoteProvider("apollo", "localhost:8080", "TEST.Namespace1")
 
 	app.ReadRemoteConfig()
 	test.ReadRemoteConfig()
